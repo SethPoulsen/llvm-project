5bfa3d3802777bbeb4188f9ddb40e89be56e671e
Samuel Grayson <sam@samgrayson.me>
2019-12-11 18:31:53 -0600

    wip

--- llvm/lib/CodeGen/RegAllocSS.cpp
+++ llvm/lib/CodeGen/RegAllocSS.cpp
@@ -1,133 +1,115 @@
-//===-- RegAllocSS.cpp - Basic Register Allocator ----------------------===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-//
-// This file defines the RegAllocSS function pass, which provides a minimal
-// implementation of the basic register allocator.
-//
-//===----------------------------------------------------------------------===//
+#include <cassert>
+#include <vector>
 
-#include "AllocationOrder.h"
 #include "LiveDebugVariables.h"
-#include "RegAllocBase.h"
 #include "Spiller.h"
-#include "llvm/Analysis/AliasAnalysis.h"
-#include "llvm/CodeGen/CalcSpillWeights.h"
+#include "llvm/ADT/StringRef.h"
+#include "llvm/CodeGen/LiveStacks.h"
 #include "llvm/CodeGen/LiveIntervals.h"
 #include "llvm/CodeGen/LiveRangeEdit.h"
 #include "llvm/CodeGen/LiveRegMatrix.h"
-#include "llvm/CodeGen/LiveStacks.h"
 #include "llvm/CodeGen/MachineBlockFrequencyInfo.h"
+#include "llvm/CodeGen/MachineFunction.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
-#include "llvm/CodeGen/MachineInstr.h"
 #include "llvm/CodeGen/MachineLoopInfo.h"
 #include "llvm/CodeGen/MachineRegisterInfo.h"
-#include "llvm/CodeGen/Passes.h"
 #include "llvm/CodeGen/RegAllocRegistry.h"
-#include "llvm/CodeGen/TargetRegisterInfo.h"
+#include "llvm/CodeGen/RegisterClassInfo.h"
+#include "llvm/CodeGen/SlotIndexes.h"
 #include "llvm/CodeGen/VirtRegMap.h"
-#include "llvm/PassAnalysisSupport.h"
-#include "llvm/Support/Debug.h"
+#include "llvm/Pass.h"
+#include "llvm/PassSupport.h"
+#include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
-#include <cstdlib>
-#include <queue>
 
 using namespace llvm;
 
 #define DEBUG_TYPE "regalloc"
 
 static RegisterRegAlloc ssRegAlloc("rass", "Sam + Seth's Register Allocator",
-                                      createSSRegisterAllocator);
+				   createSSRegisterAllocator);
 
 namespace {
-  struct CompSpillWeight {
-    bool operator()(LiveInterval *A, LiveInterval *B) const {
-      return A->weight < B->weight;
+
+  using VirtReg = unsigned int;
+
+  // https://llvm.org/doxygen/classllvm_1_1MachineFunctionPass.html
+  class RegAllocSS : public MachineFunctionPass,
+                     private LiveRangeEdit::Delegate  {
+  private:
+
+    MachineFunction* MF;
+    MachineRegisterInfo* MRI;
+    const MachineFrameInfo* MFI;
+    const TargetSubtargetInfo* STI;
+
+    const TargetRegisterInfo* TRI;
+    const TargetInstrInfo* TII;
+
+    VirtRegMap* VRM;
+    LiveIntervals* LIS;
+    const LiveRegMatrix* LRM;
+    RegisterClassInfo* RCI;
+
+    std::queue<VirtReg> regs;
+
+    std::vector<MCPhysReg> get_preferred_phys_regs(VirtReg reg);
+
+    std::vector<VirtReg> get_all_virt_regs();
+
+  public:
+
+    static char ID;
+
+    RegAllocSS() : MachineFunctionPass(ID) {}
+
+    StringRef getPassName() const override { return "SS Register Allocator"; }
+
+    void getAnalysisUsage(AnalysisUsage &AU) const {
+      // TODO: prune these
+      AU.setPreservesCFG();
+      AU.addRequired<AAResultsWrapperPass>();
+      AU.addPreserved<AAResultsWrapperPass>();
+      AU.addRequired<LiveIntervals>();
+      AU.addPreserved<LiveIntervals>();
+      AU.addPreserved<SlotIndexes>();
+      AU.addRequired<LiveDebugVariables>();
+      AU.addPreserved<LiveDebugVariables>();
+      AU.addRequired<LiveStacks>();
+      AU.addPreserved<LiveStacks>();
+      AU.addRequired<MachineBlockFrequencyInfo>();
+      AU.addPreserved<MachineBlockFrequencyInfo>();
+      AU.addRequiredID(MachineDominatorsID);
+      AU.addPreservedID(MachineDominatorsID);
+      AU.addRequired<MachineLoopInfo>();
+      AU.addPreserved<MachineLoopInfo>();
+      AU.addRequired<VirtRegMap>();
+      AU.addPreserved<VirtRegMap>();
+      AU.addRequired<LiveRegMatrix>();
+      AU.addPreserved<LiveRegMatrix>();
+      MachineFunctionPass::getAnalysisUsage(AU);
     }
+
+    MachineFunctionProperties getRequiredProperties() const override {
+      return MachineFunctionProperties().set(
+          MachineFunctionProperties::Property::NoPHIs);
+    }
+
+    bool runOnMachineFunction(MachineFunction &MF);
+
+    bool LRE_CanEraseVirtReg(unsigned) override;
+    void LRE_WillShrinkVirtReg(unsigned) override;
+
   };
-}
-
-namespace {
-/// RegAllocSS provides a minimal implementation of the basic register allocation
-/// algorithm. It prioritizes live virtual registers by spill weight and spills
-/// whenever a register is unavailable. This is not practical in production but
-/// provides a useful baseline both for measuring other allocators and comparing
-/// the speed of the basic algorithm against other styles of allocators.
-class RegAllocSS : public MachineFunctionPass,
-                public RegAllocBase,
-                private LiveRangeEdit::Delegate {
-  // context
-  MachineFunction *MF;
-
-  // state
-  std::unique_ptr<Spiller> SpillerInstance;
-  std::priority_queue<LiveInterval*, std::vector<LiveInterval*>,
-                      CompSpillWeight> Queue;
-
-  // Scratch space.  Allocated here to avoid repeated malloc calls in
-  // selectOrSplit().
-  BitVector UsableRegs;
-
-  bool LRE_CanEraseVirtReg(unsigned) override;
-  void LRE_WillShrinkVirtReg(unsigned) override;
-
-public:
-  RegAllocSS();
-
-  /// Return the pass name.
-  StringRef getPassName() const override { return "Basic Register Allocator"; }
-
-  /// RegAllocSS analysis usage.
-  void getAnalysisUsage(AnalysisUsage &AU) const override;
-
-  void releaseMemory() override;
-
-  Spiller &spiller() override { return *SpillerInstance; }
-
-  void enqueue(LiveInterval *LI) override {
-    Queue.push(LI);
-  }
-
-  LiveInterval *dequeue() override {
-    if (Queue.empty())
-      return nullptr;
-    LiveInterval *LI = Queue.top();
-    Queue.pop();
-    return LI;
-  }
-
-  unsigned selectOrSplit(LiveInterval &VirtReg,
-                         SmallVectorImpl<unsigned> &SplitVRegs) override;
-
-  /// Perform register allocation.
-  bool runOnMachineFunction(MachineFunction &mf) override;
-
-  MachineFunctionProperties getRequiredProperties() const override {
-    return MachineFunctionProperties().set(
-        MachineFunctionProperties::Property::NoPHIs);
-  }
-
-  // Helper for spilling all live virtual registers currently unified under preg
-  // that interfere with the most recently queried lvr.  Return true if spilling
-  // was successful, and append any new spilled/split intervals to splitLVRs.
-  bool spillInterferences(LiveInterval &VirtReg, unsigned PhysReg,
-                          SmallVectorImpl<unsigned> &SplitVRegs);
-
-  static char ID;
-};
-
-char RegAllocSS::ID = 0;
 
 } // end anonymous namespace
 
+char RegAllocSS::ID = 0;
 char &llvm::RegAllocSSID = RegAllocSS::ID;
 
 INITIALIZE_PASS_BEGIN(RegAllocSS, "RegAllocSS", "Sam + Seth Register Allocator",
                       false, false)
+// TODO: prune these
 INITIALIZE_PASS_DEPENDENCY(LiveDebugVariables)
 INITIALIZE_PASS_DEPENDENCY(SlotIndexes)
 INITIALIZE_PASS_DEPENDENCY(LiveIntervals)
@@ -138,16 +120,15 @@ INITIALIZE_PASS_DEPENDENCY(MachineDominatorTree)
 INITIALIZE_PASS_DEPENDENCY(MachineLoopInfo)
 INITIALIZE_PASS_DEPENDENCY(VirtRegMap)
 INITIALIZE_PASS_DEPENDENCY(LiveRegMatrix)
-INITIALIZE_PASS_END(RegAllocSS, "RegAllocSS", "Sam + Seth Register Allocator", false,
-                    false)
+INITIALIZE_PASS_END(RegAllocSS, "RegAllocSS", "Sam + Seth Register Allocator",
+		    false, false)
 
-bool RegAllocSS::LRE_CanEraseVirtReg(unsigned VirtReg) {
+bool RABasic::LRE_CanEraseVirtReg(unsigned VirtReg) {
   LiveInterval &LI = LIS->getInterval(VirtReg);
   if (VRM->hasPhys(VirtReg)) {
-    Matrix->unassign(LI);
-    aboutToRemoveInterval(LI);
     return true;
   }
+
   // Unassigned virtreg is probably in the priority queue.
   // RegAllocBase will erase it after dequeueing.
   // Nonetheless, clear the live-range so that the debug
@@ -156,178 +137,107 @@ bool RegAllocSS::LRE_CanEraseVirtReg(unsigned VirtReg) {
   return false;
 }
 
-void RegAllocSS::LRE_WillShrinkVirtReg(unsigned VirtReg) {
+void RABasic::LRE_WillShrinkVirtReg(unsigned VirtReg) {
   if (!VRM->hasPhys(VirtReg))
     return;
 
   // Register is assigned, put it back on the queue for reassignment.
-  LiveInterval &LI = LIS->getInterval(VirtReg);
-  Matrix->unassign(LI);
-  enqueue(&LI);
+  // regs.push(VirtReg);
+  VRM.assign
 }
 
-RegAllocSS::RegAllocSS(): MachineFunctionPass(ID) {
+// returns a list of possible physical registers in preferred order
+std::vector<MCPhysReg> RegAllocSS::get_preferred_phys_regs(VirtReg reg) {
+  auto array = RCI->getOrder(MRI->getRegClass(reg));
+  return std::vector<MCPhysReg> {array.begin(), array.end()};
 }
 
-void RegAllocSS::getAnalysisUsage(AnalysisUsage &AU) const {
-  AU.setPreservesCFG();
-  AU.addRequired<AAResultsWrapperPass>();
-  AU.addPreserved<AAResultsWrapperPass>();
-  AU.addRequired<LiveIntervals>();
-  AU.addPreserved<LiveIntervals>();
-  AU.addPreserved<SlotIndexes>();
-  AU.addRequired<LiveDebugVariables>();
-  AU.addPreserved<LiveDebugVariables>();
-  AU.addRequired<LiveStacks>();
-  AU.addPreserved<LiveStacks>();
-  AU.addRequired<MachineBlockFrequencyInfo>();
-  AU.addPreserved<MachineBlockFrequencyInfo>();
-  AU.addRequiredID(MachineDominatorsID);
-  AU.addPreservedID(MachineDominatorsID);
-  AU.addRequired<MachineLoopInfo>();
-  AU.addPreserved<MachineLoopInfo>();
-  AU.addRequired<VirtRegMap>();
-  AU.addPreserved<VirtRegMap>();
-  AU.addRequired<LiveRegMatrix>();
-  AU.addPreserved<LiveRegMatrix>();
-  MachineFunctionPass::getAnalysisUsage(AU);
-}
-
-void RegAllocSS::releaseMemory() {
-  SpillerInstance.reset();
-}
-
-
-// Spill or split all live virtual registers currently unified under PhysReg
-// that interfere with VirtReg. The newly spilled or split live intervals are
-// returned by appending them to SplitVRegs.
-bool RegAllocSS::spillInterferences(LiveInterval &VirtReg, unsigned PhysReg,
-                                 SmallVectorImpl<unsigned> &SplitVRegs) {
-  // Record each interference and determine if all are spillable before mutating
-  // either the union or live intervals.
-  SmallVector<LiveInterval*, 8> Intfs;
-
-  // Collect interferences assigned to any alias of the physical register.
-  for (MCRegUnitIterator Units(PhysReg, TRI); Units.isValid(); ++Units) {
-    LiveIntervalUnion::Query &Q = Matrix->query(VirtReg, *Units);
-    Q.collectInterferingVRegs();
-    for (unsigned i = Q.interferingVRegs().size(); i; --i) {
-      LiveInterval *Intf = Q.interferingVRegs()[i - 1];
-      if (!Intf->isSpillable() || Intf->weight > VirtReg.weight)
-        return false;
-      Intfs.push_back(Intf);
+std::vector<VirtReg> RegAllocSS::get_all_virt_regs() {
+  std::vector<VirtReg> out;
+  for (unsigned i = 0, e = MRI->getNumVirtRegs(); i != e; ++i) {
+    unsigned virtReg = TargetRegisterInfo::index2VirtReg(i);
+    if (!MRI->reg_nodbg_empty(virtReg)) {
+      out.push_back(virtReg);
     }
   }
-  LLVM_DEBUG(dbgs() << "spilling " << printReg(PhysReg, TRI)
-                    << " interferences with " << VirtReg << "\n");
-  assert(!Intfs.empty() && "expected interference");
-
-  // Spill each interfering vreg allocated to PhysReg or an alias.
-  for (unsigned i = 0, e = Intfs.size(); i != e; ++i) {
-    LiveInterval &Spill = *Intfs[i];
-
-    // Skip duplicates.
-    if (!VRM->hasPhys(Spill.reg))
-      continue;
-
-    // Deallocate the interfering vreg by removing it from the union.
-    // A LiveInterval instance may not be in a union during modification!
-    Matrix->unassign(Spill);
-
-    // Spill the extracted interval.
-    LiveRangeEdit LRE(&Spill, SplitVRegs, *MF, *LIS, VRM, this, &DeadRemats);
-    spiller().spill(LRE);
-  }
-  return true;
+  return out;
 }
 
-// Driver for the register assignment and splitting heuristics.
-// Manages iteration over the LiveIntervalUnions.
-//
-// This is a minimal implementation of register assignment and splitting that
-// spills whenever we run out of registers.
-//
-// selectOrSplit can only be called once per live virtual register. We then do a
-// single interference test for each register the correct class until we find an
-// available register. So, the number of interference tests in the worst case is
-// |vregs| * |machineregs|. And since the number of interference tests is
-// minimal, there is no value in caching them outside the scope of
-// selectOrSplit().
-unsigned RegAllocSS::selectOrSplit(LiveInterval &VirtReg,
-                                SmallVectorImpl<unsigned> &SplitVRegs) {
-  // Populate a list of physical register spill candidates.
-  SmallVector<unsigned, 8> PhysRegSpillCands;
+bool RegAllocSS::runOnMachineFunction(MachineFunction &MF_) {
+  ///// Get information /////
+  MF = &MF_;
+  MRI = &MF->getRegInfo();
+  MFI = &MF->getFrameInfo();
+  STI = &MF->getSubtarget();
 
-  // Check for an available register in this class.
-  AllocationOrder Order(VirtReg.reg, *VRM, RegClassInfo, Matrix);
-  while (unsigned PhysReg = Order.next()) {
-    // Check for interference in PhysReg
-    switch (Matrix->checkInterference(VirtReg, PhysReg)) {
-    case LiveRegMatrix::IK_Free:
-      // PhysReg is available, allocate it.
-      return PhysReg;
+  TRI = STI->getRegisterInfo();
+  TII = STI->getInstrInfo();
 
-    case LiveRegMatrix::IK_VirtReg:
-      // Only virtual registers in the way, we may be able to spill them.
-      PhysRegSpillCands.push_back(PhysReg);
-      continue;
+  VRM = &getAnalysis<VirtRegMap>();
+  LIS = &getAnalysis<LiveIntervals>();
+  LRM = &getAnalysis<LiveRegMatrix>();
+  RCI = new RegisterClassInfo{};
 
-    default:
-      // RegMask or RegUnit interference.
-      continue;
+  // TODO: make this happen when debug flag is on
+  dbgs() << "********** SS REGISTER ALLOCATION (spill all registers) **********\n"
+	 << "********** Function: " << MF->getName() << '\n';
+  MF->dump(); dbgs() << "\n";
+
+  ///// Instantiate relevant objects /////
+
+  // TODO: factor out the malloc for efficiency
+  std::unique_ptr<Spiller> spiller {createInlineSpiller(*this, *MF, *VRM)};
+  SmallPtrSet<MachineInstr *, 32> DeadRemats;
+  std::vector<VirtReg> virt_regs = get_all_virt_regs();
+  using VirtRegVec = SmallVector<unsigned, 4>;
+
+  ///// Preparation /////
+  MRI->freezeReservedRegs(*MF);
+  RCI->runOnMachineFunction(*MF);
+
+  for (VirtReg virt_reg : virt_regs) {
+    regs.push(virt_reg);
+  }
+
+  ///// Initial pass through virt_regs /////
+  while (!regs.empty()) {
+    Virtreg virt_reg = reg.front();
+    reg.pop();
+
+    // according to comment in RegAllocBase.cpp:
+    // > Unused registers can appear when the spiller coalesces snippets.
+    if (!MRI->reg_nodbg_empty(virt_reg)) {
+      MCPhysReg phys_reg = get_preferred_phys_regs(virt_reg)[0];
+
+      // VRM->assignVirt2StackSlot(virt_reg);
+      // VRM->assignVirt2Phys(virt_reg, phys_reg);
+
+      VirtRegVec SplitVRegs;
+      LiveInterval& LI = LIS->getInterval(virt_reg);
+      LiveRangeEdit LRE {&LI, SplitVRegs, *MF, *LIS, VRM, this, &DeadRemats};
+      spiller->spill(LRE);
     }
   }
 
-  // Try to spill another interfering reg with less spill weight.
-  for (SmallVectorImpl<unsigned>::iterator PhysRegI = PhysRegSpillCands.begin(),
-       PhysRegE = PhysRegSpillCands.end(); PhysRegI != PhysRegE; ++PhysRegI) {
-    if (!spillInterferences(VirtReg, *PhysRegI, SplitVRegs))
-      continue;
-
-    assert(!Matrix->checkInterference(VirtReg, *PhysRegI) &&
-           "Interference after spill.");
-    // Tell the caller to allocate to this newly freed physical register.
-    return *PhysRegI;
+  ///// Post optimization /////
+  spiller->postOptimization();
+  for (auto DeadInst : DeadRemats) {
+    LIS->RemoveMachineInstrFromMaps(*DeadInst);
+    DeadInst->eraseFromParent();
   }
+  DeadRemats.clear();
+  // delete spiller;
+  delete RCI;
 
-  // No other spill candidates were found, so spill the current VirtReg.
-  LLVM_DEBUG(dbgs() << "spilling: " << VirtReg << '\n');
-  if (!VirtReg.isSpillable())
-    return ~0u;
-  LiveRangeEdit LRE(&VirtReg, SplitVRegs, *MF, *LIS, VRM, this, &DeadRemats);
-  spiller().spill(LRE);
+  ///// Error reporting /////
+  // TODO: make this happen when VerifyEnabled
+  dbgs() << "Post alloc VirtRegMap:\n" << VRM << "\n";
+  dbgs() << "Verifying\n";
+  MF->verify(this, "After register allocation");
 
-  // The live virtual register requesting allocation was spilled, so tell
-  // the caller not to allocate anything during this round.
-  return 0;
-}
-
-bool RegAllocSS::runOnMachineFunction(MachineFunction &mf) {
-  LLVM_DEBUG(dbgs() << "********** BASIC REGISTER ALLOCATION **********\n"
-                    << "********** Function: " << mf.getName() << '\n');
-
-  MF = &mf;
-  RegAllocBase::init(getAnalysis<VirtRegMap>(),
-                     getAnalysis<LiveIntervals>(),
-                     getAnalysis<LiveRegMatrix>());
-
-  calculateSpillWeightsAndHints(*LIS, *MF, VRM,
-                                getAnalysis<MachineLoopInfo>(),
-                                getAnalysis<MachineBlockFrequencyInfo>());
-
-  SpillerInstance.reset(createInlineSpiller(*this, *MF, *VRM));
-
-  allocatePhysRegs();
-  postOptimization();
-
-  // Diagnostic output before rewriting
-  LLVM_DEBUG(dbgs() << "Post alloc VirtRegMap:\n" << *VRM << "\n");
-
-  releaseMemory();
   return true;
 }
 
-FunctionPass* llvm::createSSRegisterAllocator()
-{
+FunctionPass *llvm::createSSRegisterAllocator() {
   return new RegAllocSS();
 }
